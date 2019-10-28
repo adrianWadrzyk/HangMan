28175cdbc4845c7f132a4d50d2816037f909fbbe
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2019-10-28 22:54:16 +0100

    Added end of game and automatically generated board

--- game.js
+++ game.js
@@ -1,17 +1,22 @@
 var words = ['ziemia', 'zamek', 'tecza'];
-
+var point = 0;
 
 const gameOn = function() {
-    console.log("Gra się rozpoczęła")
-    let live = 5,
-        point = 0;
+
+    //clear boardConteiner for new game;
+    const boardConteiner = document.querySelector('.boardConteiner');
+    boardConteiner.innerHTML = '';
+
+    let live = 5;
+    // createGameBoard
+    createGameBoard()
 
     // Get word from Array 
     let word = words[getRandomWord(words.length)];
     console.log(word);
 
-    //createElement 
-    createElement(word.length);
+    //createFloor for word letters 
+    createFloor(word.length);
 
 
     // Get letter onClick
@@ -24,20 +29,14 @@ const gameOn = function() {
 
         if (writeLetter(letter, word)) {
             live--;
-            if (live == 0) {
-                console.log("Przegrałeś");
-            }
-        } else {
-            ++point;
-            if (point == word.length) {
-                console.log("Gratulacje, wygrałeś!");
-            }
+            if (live == 0)
+                endGame(true);
         }
     });
 }
 
 const getRandomWord = length => Math.floor(Math.random() * length);
-const createElement = length => {
+const createFloor = length => {
 
     const conteinerForWord = document.getElementById("wordConteiner");
 
@@ -51,22 +50,65 @@ const createElement = length => {
 const writeLetter = (letter, word) => {
     let miss = true;
     let tests = document.querySelectorAll(".test");
-    for (let i = 0; i < word.length; i++) {
 
+    for (let i = 0; i < word.length; i++) {
         if (letter.toLowerCase() == word[i]) {
+            ++point;
+            if (point == word.length) {
+                endGame(false);
+            }
             tests[i].innerText = letter;
             miss = false;
         }
     }
-
     return miss;
 }
 
+const createGameBoard = () => {
 
-gameOn();
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'U', 'P', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z'];
+    const boardConteiner = document.querySelector('.boardConteiner');
 
 
-// Isuses to fix 
-// How to end a game? 
-// Multiplayers mode
-// space in words;
+    // create letterConteiner
+    const letterConteiner = document.createElement('div');
+    letterConteiner.classList.add('letterConteiner');
+    boardConteiner.prepend(letterConteiner);
+
+
+    // create alphabet
+    for (let i = 0; i < alphabet.length; i++) {
+        let divLetter = document.createElement("div");
+        divLetter.classList.add("letter");
+        document.querySelector('.letterConteiner').appendChild(divLetter);
+        divLetter.innerText = alphabet[i];
+    }
+
+    // create wordConteiner
+    const wordConteiner = document.createElement('div');
+    wordConteiner.id = 'wordConteiner';
+    boardConteiner.prepend(wordConteiner);
+}
+
+
+const startButton = document.querySelector(".startGame");
+startButton.addEventListener("click", () => {
+    gameOn();
+    startButton.disabled = true;
+});
+
+const endGame = isDead => {
+        const boardConteiner = document.querySelector('.boardConteiner');
+        boardConteiner.innerHTML = '';
+        if (isDead) {
+            boardConteiner.innerText = " Przegrałeś!"
+        } else {
+            boardConteiner.innerText = 'Wygrałeś!';
+        }
+
+        const startButton = document.querySelector(".startGame");
+        startButton.disabled = false;
+    }
+    // Isuses to fix 
+    // Multiplayers mode
+    // space in words;
